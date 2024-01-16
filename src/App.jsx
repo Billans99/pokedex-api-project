a63bd086dd0b9ba11c04e17009457816ebb89059
Billy <billy.xu99@gmail.com>
2024-01-16 18:51:24 +1000

    Added text color based on pokemon type

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Cards from './Cards.jsx'
 import Footer from './Footer.jsx'
 import axios from 'axios'
 
+
 import { useEffect, useState } from 'react'
 
 const App = () => {
@@ -51,11 +52,57 @@ const App = () => {
     setUrl(nextUrl)
   }
 
+  const getTypeColor = (typeName) => {
+    switch (typeName) {
+        case "normal":
+            return "#A8A77A";
+        case "fire":
+            return "#EE8130";
+        case "water":
+            return "#6390F0";
+        case "electric":
+            return "#F7D02C";
+        case "grass":
+            return "#7AC74C";
+        case "ice":
+            return "#96D9D6";
+        case "fighting":
+            return "#C22E28";
+        case "poison":
+            return "#A33EA1";
+        case "ground":
+            return "#E2BF65";
+        case "flying":
+            return "#A98FF3";
+        case "psychic":
+            return "#F95587";
+        case "bug":
+            return "#A6B91A";
+        case "rock":
+            return "#B6A136";
+        case "ghost":
+            return "#735797";
+        case "dragon":
+            return "#6F35FC";
+        case "dark":
+            return "#705746";
+        case "steel":
+            return "#B7B7CE";
+        case "fairy":
+            return "#D685AD";
+        default:
+            return "#000000";
+    }} 
+
+
   return (
     <>
       <Header/>
       {/* <Search/> */}
-      <Cards pokemon={pokeData} loading={loading}/>
+      <Cards 
+      pokemon={pokeData} 
+      loading={loading} 
+      getTypeColor={getTypeColor} />
       <Footer 
         loadPreviousPokemon={loadPreviousPokemon} 
         loadNextPokemon={loadNextPokemon}/>
--- src/Cards.jsx
+++ src/Cards.jsx
@@ -5,9 +5,48 @@ import React from 'react'
 // Pokemon profile card: includes container, image, and type
 
 const Cards = ({ pokemon, loading }) => { 
-    console.log(pokemon)
 
-    const abilityUrlArray = []
+    const getTypeColor = (typeName) => {
+        switch (typeName) {
+            case "normal":
+                return "#A8A77A";
+            case "fire":
+                return "#EE8130";
+            case "water":
+                return "#6390F0";
+            case "electric":
+                return "#F7D02C";
+            case "grass":
+                return "#7AC74C";
+            case "ice":
+                return "#96D9D6";
+            case "fighting":
+                return "#C22E28";
+            case "poison":
+                return "#A33EA1";
+            case "ground":
+                return "#E2BF65";
+            case "flying":
+                return "#A98FF3";
+            case "psychic":
+                return "#F95587";
+            case "bug":
+                return "#A6B91A";
+            case "rock":
+                return "#B6A136";
+            case "ghost":
+                return "#735797";
+            case "dragon":
+                return "#6F35FC";
+            case "dark":
+                return "#705746";
+            case "steel":
+                return "#B7B7CE";
+            case "fairy":
+                return "#D685AD";
+            default:
+                return "#000000";
+        }} 
 
     return(
     <> 
@@ -17,14 +56,22 @@ const Cards = ({ pokemon, loading }) => {
             return(
                 <>
                     <div className="card-container"> 
+
                         <img className="pokeImage" src={item.sprites.front_default} alt="Pokemon character image">
                         </img>
+
                         <div className="info-container">
                             <h3 className="name">{item.name}</h3>
 
-                            <p className="typeInfo"> Type -
-                                {item.types.map((type) => <p>{type.type.name}</p>)}
-                            </p>
+                            <p className="typeInfo"> Type - </p>
+                                
+
+                            {item.types.map((type) => <p style={{ color: getTypeColor(type.type.name)}}>
+                                {type.type.name}
+                                </p>
+                                )}
+
+                        
 
                             <p className="abilityInfo">Abilities -
                                 {item.abilities.map((ability) => <p>{ability.ability.name}</p>)}
@@ -33,9 +80,6 @@ const Cards = ({ pokemon, loading }) => {
                             <p className="base-stats-container">Base stats -
                                 {item.stats.map((stat) => <p className="base-stats-info"> {stat.stat.name}: {stat.base_stat}</p>)}
                             </p>
-                        
-
-                            
                         </div>
                     </div> 
                 </>
